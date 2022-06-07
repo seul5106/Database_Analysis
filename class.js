11181332024926445b3e994c5f3c51ae2b36d000
NeoJHS <seul5106@gmail.com>
2022-06-07 14:16:34 +0900

    .

---
+++ class.js
@@ -0,0 +1,16 @@
+class Member{
+    constructor(name, state) {
+        this.name = name;
+        this.state = state
+    }
+
+    getName(){
+        return this
+    }
+}
+
+const date = new Date();
+const member = new Member("한슬", 123);
+
+
+console.log(member.getName())
